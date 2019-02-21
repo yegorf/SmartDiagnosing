7af5f211d02168a94f95584e554d9d010fdea857
yegor <yegorkhohlov@gmail.com>
2019-02-21 19:42:10 +0300

    Added main testing.

--- src/main/java/com/yegorf/controllers/controllers/TestController.java
+++ src/main/java/com/yegorf/controllers/controllers/TestController.java
@@ -1,7 +1,11 @@
 package com.yegorf.controllers.controllers;
 
 
+import com.yegorf.controllers.enities.Diagnose;
+import com.yegorf.controllers.enities.Matches;
 import com.yegorf.controllers.enities.Symptome;
+import com.yegorf.controllers.repos.DiagnoseRepo;
+import com.yegorf.controllers.repos.MatchesRepo;
 import com.yegorf.controllers.repos.SymptomeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
 import java.util.Map;
 
 @Controller
@@ -17,7 +22,11 @@ import java.util.Map;
 public class TestController {
 
     @Autowired
+    private DiagnoseRepo diagnoseRepo;
+    @Autowired
     private SymptomeRepo symptomeRepo;
+    @Autowired
+    private MatchesRepo matchesRepo;
 
     @GetMapping
     public String test(Map<String, Object> model) {
@@ -31,9 +40,34 @@ public class TestController {
     public String go(@RequestParam String[] list,
                      Map<String, Object> model) {
 
-        for (String s : list) {
-            System.out.println(s);
+        int COUNT = list.length;
+        int count = 0;
+
+        ArrayList<Diagnose> diagnoses = (ArrayList<Diagnose>) diagnoseRepo.findAll();
+
+        for(Diagnose d : diagnoses) {
+            ArrayList<Matches> matches = matchesRepo.findAllByDiagnose(d);
+
+            for(Matches m : matches) {
+                System.out.println(m.getDiagnose().getDiagnose() + " " + m.getSymptome().getSymptome());
+                System.out.println();
+                for(String s : list) {
+                    if(s.equals(m.getSymptome().getSymptome())) {
+                        count++;
+                    }
+                }
+            }
+            if(count == COUNT) {
+                //System.out.println("Ваш диагноз - " + d.getDiagnose());
+                //break;
+                model.put("res",d.getDiagnose());
+                return "result";
+            } else {
+               // System.out.println("Мы хз чем вы больны");
+                model.put("res", "idk");
+            }
         }
-        return "test";
+
+        return "result";
     }
 }
--- src/main/java/com/yegorf/controllers/repos/MatchesRepo.java
+++ src/main/java/com/yegorf/controllers/repos/MatchesRepo.java
@@ -3,7 +3,11 @@ package com.yegorf.controllers.repos;
 import com.yegorf.controllers.enities.Diagnose;
 import com.yegorf.controllers.enities.Match;
 import com.yegorf.controllers.enities.Matches;
+import com.yegorf.controllers.enities.Symptome;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 public interface MatchesRepo extends CrudRepository<Matches, Long> {
+    ArrayList<Matches> findAllByDiagnose(Diagnose diagnose);
 }
